4ca10f7fd9f33b659d9db9b8f7ebdf6c3e228e7b
Nick Pelikan <nick@the-fulcrum.com>
2020-01-14 14:15:37 -0700

    better data cleaning and locations

--- get_buoy_data.R
+++ get_buoy_data.R
@@ -6,6 +6,20 @@ library(readr)
 library(stringr)
 library(lubridate)
 
+decompress <- function(x) {
+    stopifnot(httr:::is.response(x))
+
+    tmp <- tempfile()
+    on.exit(unlink(tmp))
+
+    writeBin(content(x), tmp)
+
+    con <- file(tmp) # R automatically detects compression
+    open(con, "rb")
+    on.exit(close(con), TRUE)
+
+    readBin(con, raw(), file.info(tmp)$size * 10)
+}
 
 # fxn to parse buoy FWF file format
 # `rwstring`` should be the raw text file (as a string) downloaded from the NOAA
@@ -36,72 +50,70 @@ parse_buoy_data <- function(rwstring, na = c("", "NA")){
     modify_if(out, is.character, as.numeric)
 }
 
-decompress <- function(x) {
-    stopifnot(httr:::is.response(x))
 
-    tmp <- tempfile()
-    on.exit(unlink(tmp))
+get_buoy_data <- function(buoy_code, hist_years){
+    hist_data <- hist_years %>%
+        map_dfr(function(year){
+            res <- GET(
+                glue("https://www.ndbc.noaa.gov/view_text_file.php?filename={buoy_code}h{year}.txt.gz&dir=data/historical/stdmet/")
+            )
 
-    writeBin(content(x), tmp)
+            if(res$status_code == 200){
+                parse_buoy_data(content(res))
+            } else {
+                return(NULL)
+            }
 
-    con <- file(tmp) # R automatically detects compression
-    open(con, "rb")
-    on.exit(close(con), TRUE)
+        })
 
-    readBin(con, raw(), file.info(tmp)$size * 10)
+    jan_sept_19_data <- list(mnth = month.abb[1:9], mc = 1:9) %>%
+        pmap_dfr(function(mnth, mc){
+            "https://www.ndbc.noaa.gov/data/stdmet/{mnth}/{buoy_code}{mc}2019.txt.gz" %>%
+                glue() %>%
+                GET(add_headers(.headers = c('Accept-Encoding' = 'gzip, deflate'))) %>%
+                decompress() %>% rawToChar() %>%
+                parse_buoy_data()
+        })
+
+    oct19_data <- "https://www.ndbc.noaa.gov/view_text_file.php?filename={buoy_code}a2019.txt.gz&dir=data/stdmet/Oct/" %>%
+        glue() %>% GET() %>% content() %>% parse_buoy_data()
+
+    nov19_data <- "https://www.ndbc.noaa.gov/data/stdmet/Nov/{buoy_code}.txt" %>%
+        glue() %>% GET() %>% content() %>% parse_buoy_data()
+
+    current_data <- "https://www.ndbc.noaa.gov/data/realtime2/{buoy_code}.txt" %>%
+        glue() %>% GET() %>% content() %>% parse_buoy_data(na = c("", "NA", "MM"))
+
+    full_data <- bind_rows(hist_data, jan_sept_19_data, oct19_data, nov19_data,
+                           current_data)
+
+    # create date-time column
+    full_data <- full_data %>%
+        mutate(
+            datetime = make_datetime(
+                year = YY, month = MM, day = DD,
+                hour = hh, min = mm
+            )
+        )
+
+    # drop duplicates
+    full_data <- full_data %>% distinct()
+
+    # replace appropriate values with na
+    full_data <- full_data %>%
+        mutate_at(c("WSPD", "GST", "VIS", "TIDE", "DPD", "APD", "WVHT"), ~na_if(., 99)) %>%
+        mutate_at(c("WDIR", "ATMP", "WTMP", "DEWP", "MWD"), ~na_if(., 999)) %>%
+        mutate_at(c("PRES"), ~na_if(., 9999))
+
+    summary(full_data)
+
+    out <- full_data %>%
+        select(-YY, -MM, -DD, -hh, -mm) %>%
+        write_csv(glue("data/buoy/{buoy_code}.csv.gz"))
 }
 
+get_buoy_data(51101, hist_years = 2008:2018)
+get_buoy_data(51001, hist_years = c(2008, 2009, 2015:2018))
 
-hist_data <- 2008:2018 %>%
-    map_dfr(function(year){
-        res <- GET(
-            glue("https://www.ndbc.noaa.gov/view_text_file.php?filename=51101h{year}.txt.gz&dir=data/historical/stdmet/")
-        )
-        parse_buoy_data(content(res))
-    })
 
-jan_sept_19_data <- list(mnth = month.abb[1:9], mc = 1:9) %>%
-    pmap_dfr(function(mnth, mc){
-        "https://www.ndbc.noaa.gov/data/stdmet/{mnth}/51101{mc}2019.txt.gz" %>%
-            glue() %>%
-            GET(add_headers(.headers = c('Accept-Encoding' = 'gzip, deflate'))) %>%
-            decompress() %>% rawToChar() %>%
-            parse_buoy_data()
-    })
-
-oct19_data <- "https://www.ndbc.noaa.gov/view_text_file.php?filename=51101a2019.txt.gz&dir=data/stdmet/Oct/" %>%
-    GET() %>% content() %>% parse_buoy_data()
-
-nov19_data <- "https://www.ndbc.noaa.gov/data/stdmet/Nov/51101.txt" %>%
-    GET() %>% content() %>% parse_buoy_data()
-
-current_data <- "https://www.ndbc.noaa.gov/data/realtime2/51101.txt" %>%
-    GET() %>% content() %>% parse_buoy_data(na = c("", "NA", "MM"))
-
-full_data <- bind_rows(hist_data, jan_sept_19_data, oct19_data, nov19_data,
-                       current_data)
-
-# create date-time column
-full_data <- full_data %>%
-    mutate(
-        datetime = make_datetime(
-            year = YY, month = MM, day = DD,
-            hour = hh, min = mm
-        )
-    )
-
-# drop duplicates
-full_data <- full_data %>% distinct()
-
-# replace appropriate values with na
-full_data <- full_data %>%
-    mutate_at(c("WSPD", "GST", "VIS", "TIDE", "DPD", "APD", "WVHT"), ~na_if(., 99)) %>%
-    mutate_at(c("WDIR", "ATMP", "WTMP", "DEWP", "MWD"), ~na_if(., 999)) %>%
-    mutate_at(c("PRES"), ~na_if(., 9999))
-
-summary(full_data)
-
-out <- full_data %>%
-    select(-YY, -MM, -DD, -hh, -mm) %>%
-    write_csv("51101_data.csv.gz")
 
--- get_snotel_data.R
+++ get_snotel_data.R
@@ -2,23 +2,31 @@ library(dplyr)
 library(httr)
 library(readr)
 library(lubridate)
-
-raw_df <- "https://wcc.sc.egov.usda.gov/reportGenerator/view_csv/customSingleStationReport/daily/366:UT:SNTL|id=%22%22|name/POR_BEGIN,POR_END/WTEQ::value,PREC::value,TMAX::value,TMIN::value,TAVG::value,PRCP::value" %>%
-    GET() %>% content() %>%
-    read_csv(comment = "#", col_types = "Ddddddd") %>%
-    filter(Date > as.Date("2007-12-31"))
+library(glue)
 
 
-clean_df <- raw_df %>%
-    mutate(Date = as_datetime(Date)) %>%
-    rename(
-        datetime = Date,
-        SWE = "Snow Water Equivalent (in) Start of Day Values",
-        ACC = "Precipitation Accumulation (in) Start of Day Values",
-        TMAX = "Air Temperature Maximum (degF)",
-        TMIN = "Air Temperature Minimum (degF)",
-        TMEAN = "Air Temperature Average (degF)",
-        PINC = "Precipitation Increment (in)"
-    ) %>%
-    write_csv("data/366_snotel_data.csv.gz")
+get_snotel_data <- function(site_code){
+    raw_df <- "https://wcc.sc.egov.usda.gov/reportGenerator/view_csv/customSingleStationReport/daily/{site_code}:UT:SNTL|id=%22%22|name/POR_BEGIN,POR_END/WTEQ::value,PREC::value,TMAX::value,TMIN::value,TAVG::value,PRCP::value" %>%
+        glue() %>%
+        GET() %>% content() %>%
+        read_csv(comment = "#", col_types = "Ddddddd")
+
+
+    clean_df <- raw_df %>%
+        mutate(Date = as_datetime(Date)) %>%
+        rename(
+            datetime = Date,
+            SWE = "Snow Water Equivalent (in) Start of Day Values",
+            ACC = "Precipitation Accumulation (in) Start of Day Values",
+            TMAX = "Air Temperature Maximum (degF)",
+            TMIN = "Air Temperature Minimum (degF)",
+            TMEAN = "Air Temperature Average (degF)",
+            PINC = "Precipitation Increment (in)"
+        ) %>%
+        write_csv(glue("data/snotel/{site_code}.csv.gz"))
+}
+
+get_snotel_data(366)
+get_snotel_data(766)
+
 
