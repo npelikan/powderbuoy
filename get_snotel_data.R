fe3257e08b6c2f010cf89a0e89a46e0f4a466025
Nick Pelikan <nick@the-fulcrum.com>
2020-01-14 12:17:59 -0700

    snotel data script and results

---
+++ get_snotel_data.R
@@ -0,0 +1,24 @@
+library(dplyr)
+library(httr)
+library(readr)
+library(lubridate)
+
+raw_df <- "https://wcc.sc.egov.usda.gov/reportGenerator/view_csv/customSingleStationReport/daily/366:UT:SNTL|id=%22%22|name/POR_BEGIN,POR_END/WTEQ::value,PREC::value,TMAX::value,TMIN::value,TAVG::value,PRCP::value" %>%
+    GET() %>% content() %>%
+    read_csv(comment = "#", col_types = "Ddddddd") %>%
+    filter(Date > as.Date("2007-12-31"))
+
+
+clean_df <- raw_df %>%
+    mutate(Date = as_datetime(Date)) %>%
+    rename(
+        datetime = Date,
+        SWE = "Snow Water Equivalent (in) Start of Day Values",
+        ACC = "Precipitation Accumulation (in) Start of Day Values",
+        TMAX = "Air Temperature Maximum (degF)",
+        TMIN = "Air Temperature Minimum (degF)",
+        TMEAN = "Air Temperature Average (degF)",
+        PINC = "Precipitation Increment (in)"
+    ) %>%
+    write_csv("data/366_snotel_data.csv.gz")
+
